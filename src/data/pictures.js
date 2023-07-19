e9e118a39e62ae0f7798491bb05b02cd89a30b63
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-07-18 17:06:14 -0700

    more text to slideshow

--- src/data/pictures.js
+++ src/data/pictures.js
@@ -2,7 +2,7 @@ export const Gateway = {
   date: "1998",
   description: {
     de: "Loved the family Gateway!",
-    en: "Loved the family Gateway!",
+    en: `Loved the family Gateway! The highlights were the SkiFree, Space Pinball and encyclopedia Britanica. I remember sitting in front of the beige beauty until my eyelids grew heavy. I was also obsessed with Microsoft Bob. ¯\\_(ツ)_/¯`,
     es: "Loved the family Gateway!",
     se: "Loved the family Gateway!",
   },
@@ -14,7 +14,7 @@ export const GoogleGlass = {
   date: "2014",
   description: {
     de: "OK Google - let's bring Glasses back, we are ready this time.",
-    en: "OK Google - let's bring Glasses back, we are ready this time.",
+    en: "OK Google - let's bring Glasses back, we are ready this time. It's hard to believe how far ahead of it's time the Google Glass was. I remember putting them on and being blown away, but couldn't afford the $1,500 price tag.",
     es: "OK Google - let's bring Glasses back, we are ready this time.",
     se: "OK Google - let's bring Glasses back, we are ready this time.",
   },
@@ -26,7 +26,7 @@ export const IOT = {
   date: "2019",
   description: {
     de: "Working on IOT sensors using Interstacks and Azure.",
-    en: "Working on IOT sensors using Interstacks and Azure.",
+    en: "At BCG I worked on an IOT prototype to bring to our clients. This was my first foray into the intersection of hardware and software. We used Interstacks IOT devices and Microsoft Azure IOT hub as well as a fun miniature assembly line that was 3D printed after this picture was taken.",
     es: "Working on IOT sensors using Interstacks and Azure.",
     se: "Working on IOT sensors using Interstacks and Azure.",
   },
@@ -38,7 +38,7 @@ export const MagicLeap = {
   date: "2019",
   description: {
     de: "First time creating AR application using MagicLeap. No one told me I would need to learn Unity.",
-    en: "First time creating AR application using MagicLeap. No one told me I would need to learn Unity.",
+    en: "At BCG, I had the freedom to explore use cases for AR. This is a picture of the first time creating AR application using MagicLeap. It was a sample app that tracked eye, head, and hand movement as well as demonstrated MagicLeap's spatial sound capabilities.",
     es: "First time creating AR application using MagicLeap. No one told me I would need to learn Unity.",
     se: "First time creating AR application using MagicLeap. No one told me I would need to learn Unity.",
   },
